0ce90ff7f4216308810b428b17be4be5da90834b
lonebots <jishnusurajila@gmail.com>
2022-10-14 00:41:57 +0530

    config and dependencies updated

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,6 @@
 export default {
-    port : 3000,
-    host : 'localhost',
-    dbUri : 'http://localhost:27017/rest-api'
-}
+  port: 3000,
+  host: "localhost",
+  dbUri:
+    "mongodb+srv://jishnu:malu@cluster0.hdp4pop.mongodb.net/?retryWrites=true&w=majority",
+};
