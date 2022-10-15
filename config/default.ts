202d41e014acf72b56838a93d7f2c8bdd84f5ab7
lonebots <jishnusurajila@gmail.com>
2022-10-15 14:46:46 +0530

    updated gitignore file

--- config/default.ts
+++ config/default.ts
@@ -2,5 +2,5 @@ export default {
   port: 3000,
   host: "localhost",
   dbUri:
-    "mongodb+srv://jishnu:malu@cluster0.hdp4pop.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://testuser:KnQdo2Np2BrNit2z@cluster0.hdp4pop.mongodb.net/rest-api-app?retryWrites=true&w=majority",
 };
