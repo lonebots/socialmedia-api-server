d0badf15dd1f0de95466f7dc24f28de60cb70376
Jishnu <jishnusurajila@gmail.com>
2022-10-15 14:46:27 +0530

    Update default.ts

--- config/default.ts
+++ config/default.ts
@@ -2,5 +2,5 @@ export default {
   port: 3000,
   host: "localhost",
   dbUri:
-    "mongodb+srv://jishnu:malu@cluster0.hdp4pop.mongodb.net/?retryWrites=true&w=majority",
+    "your_dbUri_should_be_added_here",
 };
