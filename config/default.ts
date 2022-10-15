82c2d74ec91d77cb2e5c66ac5bb1dc2932b1e1d4
Jishnu <jishnusurajila@gmail.com>
2022-10-15 14:59:32 +0530

    Update default.ts

--- config/default.ts
+++ config/default.ts
@@ -2,7 +2,5 @@ export default {
   port: 3000,
   host: "localhost",
   dbUri:
-    "mongodb+srv://testuser:KnQdo2Np2BrNit2z@cluster0.hdp4pop.mongodb.net/rest-api-app?retryWrites=true&w=majority",
+    "fill_in_with_your_database_uri",
 };
-
-//"mongodb+srv://testuser:KnQdo2Np2BrNit2z@cluster0.hdp4pop.mongodb.net/rest-api-app?retryWrites=true&w=majority"
