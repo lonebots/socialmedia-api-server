3b6bc71cbf9ad9755e277ff265042aaeec63f9c8
lonebots <jishnusurajila@gmail.com>
2022-10-15 20:22:16 +0530

    updated route file

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,16 @@
 import { Express, Request, Response } from "express";
 
 export default function (app: Express) {
+  // healthcheck route
   app.get("/healthcheck", (req: Request, res: Response) => {
     res.sendStatus(200);
   });
+
+  // register user
+
+  // user login 
+
+  // user session 
+
+  // user logout
 }
