636aad553858da4949dc3fb4b3c7c5b31ecc5843
lonebots <jishnusurajila@gmail.com>
2022-10-16 15:54:23 +0530

    added user schema

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,19 @@
+// used for all the user endpoints
+import { object, string, ref } from "yup";
+
+export const createUserSchema = object({
+  body: object({
+    name: string().required("Name is required"),
+    password: string()
+      .required("Password is required")
+      .min(6, "Password is too short - should be 6 chars minimum")
+      .matches(/^[a-zA-Z0-9_.-]*$/, "Password can only contain Latin letters."),
+    passwordConfirmation: string().oneOf(
+      [ref("password"), null],
+      "Password must match"
+    ),
+    email: string()
+      .required("Email is required.")
+      .email("Must be a valid email"),
+  }),
+});
