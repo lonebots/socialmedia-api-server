17d42f07b8df09c59e888624fd5f40c7a6a0417c
lonebots <jishnusurajila@gmail.com>
2022-10-16 15:54:46 +0530

    added user creation services

---
+++ src/service/user.service.ts
@@ -0,0 +1,17 @@
+import { DocumentDefinition, Error } from "mongoose";
+ import User, { UserDocument } from "../model/user.model";
+
+//
+export async function createUser(input: DocumentDefinition<UserDocument>) {
+  try {
+    return await User.create(input);
+  } catch (error : any) {
+    throw new Error(error);
+  }
+}
+
+//
+function findUser() {
+    createUser
+}
+ 
