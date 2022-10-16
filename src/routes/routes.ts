680286681a388fd4bf63c685ebaec927ccb83176
lonebots <jishnusurajila@gmail.com>
2022-10-16 15:52:52 +0530

    updated routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,7 @@
 import { Express, Request, Response } from "express";
+import {createUserHandler} from '../controller/user.controller';
+import validateRequest from '../middleware/validateRequest'
+import { createUserSchema } from "../schema/user.schema";
 
 export default function (app: Express) {
   // healthcheck route
@@ -7,10 +10,12 @@ export default function (app: Express) {
   });
 
   // register user
+  //validation is done -- inorde to ensure security and related stuffs.
+  app.post("/api/users", validateRequest(createUserSchema), createUserHandler);
 
-  // user login 
+  // user login
 
-  // user session 
+  // user session
 
   // user logout
 }
