39c3708633dd32e81c1fbc33ea29b92615831f54
lonebots <jishnusurajila@gmail.com>
2022-10-18 15:40:51 +0530

    update middleware --> added ( requires user -- check for user in incoming request)

---
+++ src/middleware/requiresUser.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { get } from "lodash";
+
+const requiresUser = (req: Request, res: Response, next: NextFunction) => {
+  const user = get(req, "user");
+
+  if (!user) {
+    return res.sendStatus(403);
+  }
+  return next();
+};
+
+export default requiresUser;
