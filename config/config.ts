1c9fcd36a5ea23dccdfaf40a4742665ee7a3cd7c
lonebots <jishnusurajila@gmail.com>
2022-10-18 14:44:32 +0530

    update config file --> added (accessTokenTtl,refreshTokenTtl,privateKey)

--- config/config.ts
+++ config/config.ts
@@ -1,8 +1,11 @@
-//rename the file as default.ts before running the server
-
 export default {
   port: 3000,
   host: "localhost",
   dbUri: "update_your_db_uri",
   saltWorkFactor: 10,
+  accessTokenTtl: "15m",
+  refreshTokenTtl: "1y",
+  privateKey: `-----BEGIN RSA PRIVATE KEY-----
+  blah....blah...blah --> update with your private key
+  -----END CERTIFICATE-----`,
 };
