70e566ba540dae29059de8a85fd913bc7a483d13
lonebots <jishnusurajila@gmail.com>
2022-10-18 15:39:11 +0530

    update app file --> added desializeuser method on all routes

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,13 @@ import config from "config";
 import log from "./logger";
 import connect from "./db/connect";
 import routes from "./routes/routes";
-//import { deserializeUser } from "./middleware";
+import { deserializeUser } from "./middleware";
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
 
 const app = express();
-//app.use(deserializeUser);
+app.use(deserializeUser); // this will attach a user to every single request that comes into the application.
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
