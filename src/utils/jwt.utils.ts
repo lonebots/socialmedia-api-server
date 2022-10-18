f1078179402c341c6484bf444c8e33ae5ae9291e
lonebots <jishnusurajila@gmail.com>
2022-10-18 13:44:55 +0530

    updated jwt-utils added -- decode function --> validate token expiry

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,8 +1,28 @@
 import jwt from "jsonwebtoken";
 import config from "config";
+import log from "../logger";
 
 const privateKey = config.get("privateKey") as string;
 
 export function sign(object: object, options?: jwt.SignOptions | undefined) {
   return jwt.sign(object, privateKey, options);
+}
+
+// decode util -- for labeling the accessToken as expire and not expired
+// defined as a wrapper around ==> jwt.verify
+export function decode(token: string) {
+  try {
+    // verify with app privateKey
+    const decoded = jwt.verify(token, privateKey);
+
+    // if no error then the token is declared as valid
+    return { valid: true, expired: false, decoded };
+  } catch (error: any) {
+    log.error(error);
+    return {
+      valid: false,
+      expired: error.msg === "jwt-expired",
+      decoded: null,
+    };
+  }
 }
