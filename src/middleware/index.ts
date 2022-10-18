9b508c35217b176303685f56c8db168d5a17ac67
lonebots <jishnusurajila@gmail.com>
2022-10-18 15:41:20 +0530

    update middleware --> added index file for easy default exports

---
+++ src/middleware/index.ts
@@ -0,0 +1,5 @@
+// import export trick using js
+
+export { default as validateRequest } from "./validateRequest";
+export { default as deserializeUser } from "./deserializeUser";
+export { default as requiresUser } from "./requiresUser";
